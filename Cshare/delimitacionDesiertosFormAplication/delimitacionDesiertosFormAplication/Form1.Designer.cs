a4e576f45794bd3199be544c917589f35ad1219c
Christian Daniel Medrano Kantuta <117957847+danyclaber@users.noreply.github.com>
2024-12-09 00:01:11 -0400

    Add files via upload

---
+++ Cshare/delimitacionDesiertosFormAplication/delimitacionDesiertosFormAplication/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace delimitacionDesiertosFormAplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 68);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(540, 525);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(467, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(285, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Delimitación de Áreas Desérticas";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(678, 68);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(539, 525);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(558, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Cargar Imagen";
+            this.button1.UseMnemonic = false;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(558, 108);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 35);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Procesar Imagen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(558, 149);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(114, 35);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Reiniciar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(256, 50);
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(1229, 605);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Cshare/delimitacionDesiertosFormAplication/delimitacionDesiertosFormAplication/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace delimitacionDesiertosFormAplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MostrarRangoDeColores()
+        {
+            // Crear un Bitmap para representar el rango de colores
+            Bitmap rangoColores = new Bitmap(256, 50); // Ancho y alto del rango
+
+            // Definir los rangos de colores
+            int minR = 150, maxR = 255;
+            int minG = 100, maxG = 200;
+            int minB = 50, maxB = 150;
+
+            for (int x = 0; x < rangoColores.Width; x++)
+            {
+                for (int y = 0; y < rangoColores.Height; y++)
+                {
+                    // Escalar los valores de R, G y B a lo largo del ancho del PictureBox
+                    int r = minR + (x * (maxR - minR) / rangoColores.Width);
+                    int g = minG + (x * (maxG - minG) / rangoColores.Width);
+                    int b = minB + (x * (maxB - minB) / rangoColores.Width);
+
+                    // Aplicar el color al píxel
+                    rangoColores.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+
+            // Mostrar el rango de colores en un PictureBox adicional
+            pictureBox3.Image = rangoColores;
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Configuración del filtro para archivos de imagen
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Imagenes PNG|*.png|Imagenes JPG|*.jpg|Todos los archivos|*.*";
+
+            // Mostrar el cuadro de diálogo para seleccionar un archivo
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Cargar la imagen seleccionada en el PictureBox
+                    Bitmap imagenCargada = new Bitmap(openFileDialog.FileName);
+                    pictureBox1.Image = imagenCargada;
+                }
+                catch (Exception ex)
+                {
+                    // Manejar errores en caso de que no se pueda cargar la imagen
+                    MessageBox.Show("Error al cargar la imagen: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+      
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap original = new Bitmap(pictureBox1.Image); // Imagen original cargada
+            Bitmap procesada = new Bitmap(original.Width, original.Height); // Imagen de salida
+
+            // Definir los rangos de colores para áreas desérticas
+            int minR = 150, maxR = 255;
+            int minG = 100, maxG = 200;
+            int minB = 50, maxB = 150;
+
+            for (int i = 0; i < original.Width; i++)
+            {
+                for (int j = 0; j < original.Height; j++)
+                {
+                    // Obtener el color del píxel actual
+                    Color pixel = original.GetPixel(i, j);
+
+                    // Verificar si el píxel está dentro del rango de colores desérticos
+                    if (pixel.R >= minR && pixel.R <= maxR &&
+                        pixel.G >= minG && pixel.G <= maxG &&
+                        pixel.B >= minB && pixel.B <= maxB)
+                    {
+                        // Marcar como área desértica (ej. en rojo)
+                        procesada.SetPixel(i, j, Color.Red);
+                    }
+                    else
+                    {
+                        // Mantener el color original
+                        procesada.SetPixel(i, j, pixel);
+                    }
+                }
+            }
+
+            // Mostrar la imagen procesada en un PictureBox
+            pictureBox2.Image = procesada;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = null; // Borra la imagen procesada
+            pictureBox1.Image = null; // Borra la imagen original
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MostrarRangoDeColores();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+    }
+}
